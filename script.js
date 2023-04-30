6bb24b5bbdb59fc98b363d11e6f992c3a4b6b0db
CardsClaw <joshr1979@gmail.com>
2023-04-29 20:23:38 -0500

    removed commenting from stopping starter code for test

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
 
