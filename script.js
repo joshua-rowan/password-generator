5f3f04cdc9bd6844d03bb528d3246ebc56167fc4
CardsClaw <joshr1979@gmail.com>
2023-05-01 15:09:08 -0500

    created the lists of characters

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
 
 function generatePassword() {
   console.log("Hey, you clicked the button!")
@@ -20,7 +24,6 @@ function generatePassword() {
   //5. Generate password
   //6. Display passowrd to the page
     }
-
 
   return "Generated password will go here!";
 }
